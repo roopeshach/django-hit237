3438b0c4413e757c263f2965ab39bcf57958b08e
Roopesh Acharya <rupeshacharya2014@gmail.com>
2023-05-23 03:36:13 +0545

    added django models Student , WasteType and FoodWaste

--- Item/models.py
+++ Item/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=254)
+    roll = models.CharField(max_length=254)
+    image = models.ImageField(upload_to='students')
+
+    def __str__(self):
+        return self.name
+    
+class WasteType(models.Model):
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    image = models.ImageField(upload_to='waste_type')
+
+    def __str__(self):
+        return self.name
+    
+class FoodWaste(models.Model):
+    name = models.CharField(max_length=254)
+    type = models.ForeignKey(WasteType, on_delete=models.CASCADE)
+    description = models.TextField()
+    source = models.CharField(max_length=254)
+    where_to_throw = models.CharField(max_length=254)
+    image = models.ImageField(upload_to='food_waste')
+    time_to_decay = models.CharField(max_length=254)
+    carbon_footprint = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.name
+    
+
+    
